29eacc94aee92d7fb112a804614d940e455ed09d
Ying WANG <ying.wang@grafana.com>
2023-10-11 17:57:41 +0200

    Distributor: Add Retry-After header for 429 response

--- pkg/ingester/ingester_test.go
+++ pkg/ingester/ingester_test.go
@@ -6033,6 +6033,7 @@ func TestIngester_PushInstanceLimitsWithCircuitBreaker(t *testing.T) {
 				Log: logger,
 			}
 			grpcOptions := []grpc.ServerOption{
+				grpc.MaxConcurrentStreams(math.MaxUint32 - 1),
 				grpc.UnaryInterceptor(middleware.ServerUserHeaderInterceptor),
 				grpc.ChainUnaryInterceptor(serverLog.UnaryServerInterceptor),
 			}
