cadd6f98450ed0726fc8c6348aadb4a5b6e32848
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-11-02 13:02:17 +0100

    include cache params in TestHandler_ServeHTTP
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/frontend/transport/handler_test.go
+++ pkg/frontend/transport/handler_test.go
@@ -179,7 +179,7 @@ func TestHandler_ServeHTTP(t *testing.T) {
 				require.Len(t, logger.logMessages, 1)
 
 				msg := logger.logMessages[0]
-				require.Len(t, msg, 18+len(tt.expectedParams))
+				require.Len(t, msg, 20+len(tt.expectedParams))
 				require.Equal(t, level.InfoValue(), msg["level"])
 				require.Equal(t, "query stats", msg["msg"])
 				require.Equal(t, "query-frontend", msg["component"])
